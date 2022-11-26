8a00a34e17c444934bf81ae2441532d7c65b66a6
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-11-26 13:49:17 +0300

    Fix data fetch bug

--- src/Redux/Greetings_Reducer.js
+++ src/Redux/Greetings_Reducer.js
@@ -2,33 +2,23 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const loadGreeting = createAsyncThunk('greetings/loadGreeting', async () => {
-  const response = await axios.get('http://localhost:3000/api/v1/greetings').catch((err) => {
+  const response = await axios.get('http://localhost:3001/api/v1/greetings').catch((err) => {
     console.log('Error', err);
   });
-  return response;
+  const res = response.data
+  return res;
 });
 
 export const greetingsSlice = createSlice({
   name: 'greetings',
-  initialState: {
-    greetings: [],
-    error: "Error, no Greetings"
-  },
+  initialState: [],
   reducers: {},
   extraReducers: {
-    [loadGreeting.pending]: (state, action) => {
-      state.status = 'loading';
-    },
     [loadGreeting.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
-      state.greetings[0] = action.payload;
+      const newState = action.payload.greeting
+      return newState;
     },
-
-    [loadGreeting.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.error.message;
-    },
-  },
+  }
 });
 
 export default greetingsSlice.reducer;
